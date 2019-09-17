476fbfefa7d9a3c827eb885d3f1ccdcd58b06f17
Felipe Andrade <lippeandrade@live.com>
2019-09-17 20:31:56 -0300

    Create contact_page.dart

---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+
+  ContactPage();
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
