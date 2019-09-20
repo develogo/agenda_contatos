34c8ade676e64ca67a7ab189a76cc471aa7d3f35
Felipe Andrade <lippeandrade@live.com>
2019-09-20 08:46:20 -0300

    Construção de Tela individual de contato

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,16 +1,106 @@
+import 'dart:io';
+
+import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class ContactPage extends StatefulWidget {
 
-  ContactPage();
+  final Contact contact;
+
+  ContactPage({this.contact});
 
   @override
   _ContactPageState createState() => _ContactPageState();
 }
 
 class _ContactPageState extends State<ContactPage> {
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  bool _userEdited = false;
+
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if(widget.contact == null){
+      _editedContact = Contact();
+    } else{
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _emailController.text = _editedContact.phone;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+        onPressed: (){} ,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null ?
+                      FileImage(File(_editedContact.img)) :
+                      AssetImage("images/person.png")
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editedContact.name =  text;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "Email"),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.email =  text;
+              },
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(labelText: "Phone"),
+              keyboardType: TextInputType.phone,
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.phone =  text;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
 }
